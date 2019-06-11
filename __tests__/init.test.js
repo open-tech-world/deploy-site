097acc73d9f6f61bd46efef09a69806d922395a2
Thanga Ganapathy <ganapathy888@gmail.com>
2019-06-12 01:11:04 +0530

    Add create module for init command

--- __tests__/init.test.js
+++ __tests__/init.test.js
@@ -1,3 +1,20 @@
-test('jest', () => {
-  expect(5 + 3).toBe(8);
+const { existsSync } = require('fs');
+
+const create = require('../lib/init/create');
+
+describe('create deploy.json file', () => {
+  test('the empty params fails with an error', async () => {
+    expect.assertions(1);
+    await expect(create({})).rejects.toThrow();
+  });
+
+  test('the invalid params fails with an error', async () => {
+    expect.assertions(1);
+    await expect(create({ provider: 'unknown' })).rejects.toThrow();
+  });
+
+  it('creates a s3 config file', async () => {
+    await create({ provider: 's3' });
+    expect(existsSync).toBeTruthy();
+  });
 });
