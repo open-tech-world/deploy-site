01d184d83b8d6c051f3116fab643dc0ca2b2275f
Thanga Ganapathy <ganapathy888@gmail.com>
2019-06-13 00:14:42 +0530

    Add lib named test file

---
+++ __tests__/deploySite.test.js
@@ -0,0 +1,45 @@
+const { existsSync } = require('fs');
+
+const init = require('../lib/init/create');
+const build = require('../lib/build');
+
+describe('create deploy.json file', () => {
+  test('the empty params fails with an error', async () => {
+    expect.assertions(1);
+    await expect(init({})).rejects.toThrow();
+  });
+
+  test('the invalid params fails with an error', async () => {
+    expect.assertions(1);
+    await expect(init({ provider: 'unknown' })).rejects.toThrow();
+  });
+
+  it('creates a s3 config file with staging & production env', async () => {
+    await init(
+      { provider: 's3', env: ['staging', 'production'] },
+      '__tests__/react-app'
+    );
+    expect(existsSync('__tests__/react-app/deploy.json')).toBeTruthy();
+  });
+});
+
+describe('build app', () => {
+  test('the empty params fails with an error', async () => {
+    expect.assertions(1);
+    await expect(build({})).rejects.toThrow();
+  });
+
+  test('the invalid provider fails with an error', async () => {
+    expect.assertions(1);
+    await expect(build('unknown')).rejects.toThrow(
+      'No such provider(unknown) found'
+    );
+  });
+
+  test('the invalid env fails with an error', async () => {
+    expect.assertions(1);
+    await expect(build('s3', 'test')).rejects.toThrow(
+      'No such environment(test) found'
+    );
+  });
+});
