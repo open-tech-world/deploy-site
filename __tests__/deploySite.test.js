5f7af9e791d1459de4f6861694c8eaa395af886b
Thanga Ganapathy <ganapathy888@gmail.com>
2019-06-13 12:54:37 +0530

    Use execa to run the build script

--- __tests__/deploySite.test.js
+++ __tests__/deploySite.test.js
@@ -1,10 +1,12 @@
 const { existsSync } = require('fs');
+const path = require('path');
 
 const init = require('../lib/init/create');
 const parseConfig = require('../lib/build/parseConfig');
 const build = require('../lib/build');
 
-const configFile = '__tests__/react-app/deploy.json';
+const reactAppPath = path.join('__tests__', 'react-app');
+const configFile = path.join(reactAppPath, 'deploy.json');
 
 describe('create deploy.json file', () => {
   test('the empty params fails with an error', async () => {
@@ -18,7 +20,10 @@ describe('create deploy.json file', () => {
   });
 
   it('creates a s3 config file with staging & production env', async () => {
-    await init({ provider: 's3', env: 'staging, production' }, configFile);
+    await init(
+      { provider: 's3', env: 'staging, production', buildDir: 'build' },
+      reactAppPath
+    );
     expect(existsSync(configFile)).toBeTruthy();
     const config = await parseConfig(configFile);
     expect(config).toHaveProperty('s3');
@@ -35,15 +40,27 @@ describe('build app', () => {
 
   test('the invalid provider fails with an error', async () => {
     expect.assertions(1);
-    await expect(build('unknown', '', configFile)).rejects.toThrow(
+    await expect(build('unknown', '', reactAppPath)).rejects.toThrow(
       'No such provider(unknown) found'
     );
   });
 
   test('the invalid env fails with an error', async () => {
     expect.assertions(1);
-    await expect(
-      build('s3', 'test', '__tests__/react-app/deploy.json')
-    ).rejects.toThrow('No such environment(test) found');
+    await expect(build('s3', 'test', reactAppPath)).rejects.toThrow(
+      'No such environment(test) found'
+    );
   });
+
+  it('creates the react app staging build', async () => {
+    const { code } = await build('s3', 'staging', reactAppPath);
+    expect(existsSync(path.join(reactAppPath, 'build'))).toBeTruthy();
+    expect(code).toBe(0);
+  }, 10000);
+
+  it('creates the react app production build', async () => {
+    const { code } = await build('s3', 'production', reactAppPath);
+    expect(existsSync(path.join(reactAppPath, 'build'))).toBeTruthy();
+    expect(code).toBe(0);
+  }, 10000);
 });
