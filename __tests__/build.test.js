bc087bece2d37722755ff39280e1794270578f94
Thanga Ganapathy <ganapathy888@gmail.com>
2019-06-18 18:05:01 +0530

    Split test files

--- __tests__/build.test.js
+++ __tests__/build.test.js
@@ -3,7 +3,6 @@ const path = require('path');
 const { remove } = require('fs-extra');
 
 const init = require('../lib/init/create');
-const parseConfig = require('../lib/build/parseConfig');
 const build = require('../lib/build');
 
 const reactAppPath = path.join('__tests__', 'react-app');
@@ -18,30 +17,6 @@ beforeAll(() => {
   return cleanUp();
 });
 
-describe('create deploy.json file', () => {
-  test('the empty params fails with an error', async () => {
-    expect.assertions(1);
-    await expect(init({})).rejects.toThrow();
-  });
-
-  test('the invalid params fails with an error', async () => {
-    expect.assertions(1);
-    await expect(init({ provider: 'unknown' })).rejects.toThrow();
-  });
-
-  it('creates a s3 config file with staging & production env', async () => {
-    await init(
-      { provider: 's3', env: 'staging, production', buildDir: 'build' },
-      reactAppPath
-    );
-    expect(existsSync(configFile)).toBeTruthy();
-    const config = await parseConfig(configFile);
-    expect(config).toHaveProperty('s3');
-    expect(config).toHaveProperty('s3.staging');
-    expect(config).toHaveProperty('s3.production');
-  });
-});
-
 describe('build app', () => {
   test('the empty params fails with an error', async () => {
     expect.assertions(1);
@@ -50,6 +25,10 @@ describe('build app', () => {
 
   test('the invalid provider fails with an error', async () => {
     expect.assertions(1);
+    await init(
+      { provider: 's3', env: 'staging, production', buildDir: 'build' },
+      reactAppPath
+    );
     await expect(build('unknown', '', reactAppPath)).rejects.toThrow(
       'No such provider(unknown) found'
     );
---
+++ __tests__/init.test.js
@@ -0,0 +1,42 @@
+const { existsSync } = require('fs');
+const path = require('path');
+const { remove } = require('fs-extra');
+
+const init = require('../lib/init/create');
+const parseConfig = require('../lib/build/parseConfig');
+
+const reactAppPath = path.join('__tests__', 'react-app');
+const configFile = path.join(reactAppPath, 'deploy.json');
+
+async function cleanUp() {
+  await remove(path.join(reactAppPath, 'build'));
+  await remove(configFile);
+}
+
+beforeAll(() => {
+  return cleanUp();
+});
+
+describe('create deploy.json file', () => {
+  test('the empty params fails with an error', async () => {
+    expect.assertions(1);
+    await expect(init({})).rejects.toThrow();
+  });
+
+  test('the invalid params fails with an error', async () => {
+    expect.assertions(1);
+    await expect(init({ provider: 'unknown' })).rejects.toThrow();
+  });
+
+  it('creates a s3 config file with staging & production env', async () => {
+    await init(
+      { provider: 's3', env: 'staging, production', buildDir: 'build' },
+      reactAppPath
+    );
+    expect(existsSync(configFile)).toBeTruthy();
+    const config = await parseConfig(configFile);
+    expect(config).toHaveProperty('s3');
+    expect(config).toHaveProperty('s3.staging');
+    expect(config).toHaveProperty('s3.production');
+  });
+});
