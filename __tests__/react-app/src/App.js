97c2427283bda9fdd701d2addfff27bf2c8ad2cf
Thanga Ganapathy <ganapathy888@gmail.com>
2019-06-19 15:33:32 +0530

    Empty s3 bucket before uploading files

--- __tests__/react-app/src/App.js
+++ __tests__/react-app/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import welcome from './undraw_welcome_3gvl.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to {process.env.REACT_APP_NAME} ({process.env.REACT_APP_BUILD_ENV})</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Welcome to {process.env.REACT_APP_NAME} ({process.env.REACT_APP_BUILD_ENV})</h1>
+      <img src={welcome} alt="welcome" />
       </header>
     </div>
   );
