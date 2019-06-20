3a3b5a4ce39ec9580888ef0a9c16f387782dd3af
Thanga Ganapathy <ganapathy888@gmail.com>
2019-06-20 12:17:39 +0530

    Add run command to bin

--- cli.js
+++ cli.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-require('./lib/')();
+const runCommand = require('./lib/runCommand');
+const deploySite = require('./lib');
+
+const args = process.argv.slice(2);
+const cmd = (args[0] || '').trim();
+
+if (cmd.charAt(0) === '-') {
+  runCommand(cmd);
+} else {
+  deploySite(args[0], args[1], args[2]);
+}
